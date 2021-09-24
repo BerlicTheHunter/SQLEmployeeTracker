32aa169016b4a4460dac135cd7e129e0a982675a
Berlic <bsorrell3@gmail.com>
2021-09-23 23:21:52 -0400

    finalized CLI

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const { Console } = require("console");
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    user:'', // 'mysql user here,
-    password:'' ,//'mysql password here,
+    user:"" , // 'mysql user here,
+    password:"", //'mysql password here,
     database: 'tracker_db'
   },
   console.log(`Database Connection Complete`)
 );
-
+// Main Pormpt Menu
 const mainMenuPrompt = () => {
   inquirer
   .prompt([
@@ -45,7 +45,10 @@ const mainMenuPrompt = () => {
         break;
 
       case "View All Employees":
-        db.query('SELECT employee.id, employee.first_name, employee.last_name, roles.title, roles.salary, department.department_name FROM employee JOIN roles ON employee.roles_id = roles.id JOIN department ON roles.department_id = department.id', async function (err, results) {
+        db.query('SELECT employee.id, employee.first_name, employee.last_name, CONCAT(m.first_name," ", m.last_name) AS manager , roles.title, roles.salary, department.department_name FROM employee JOIN roles ON employee.roles_id = roles.id JOIN department ON roles.department_id = department.id LEFT JOIN employee m ON m.id = employee.manager_id ', async function (err, results) {
+          if(err){
+            console.log(err);
+          }
           console.table(results);
           await mainMenuPrompt();
           return;
@@ -60,13 +63,21 @@ const mainMenuPrompt = () => {
         addRole();
         break;
 
+      case "Add An Employee":
+        addEmployee(); 
+        break;
+
+      case "Update An Employee":
+        updateEmployee();
+        break;  
+
       default:
         console.log('other choice');
         break;
     };
   });
 };
-
+// SQL Query to Add a Department
 const addDepartment = () => {
  inquirer
  .prompt([
@@ -95,7 +106,7 @@ const addDepartment = () => {
    });
   });
 };
-
+// SQL Query to Add a Role
 const addRole = () => {
   inquirer
   .prompt([
@@ -116,7 +127,7 @@ const addRole = () => {
     }
   ])
   .then((response) =>{
-    db.query(`INSERT INTO roles (title,salary,department_id) VALUES ("`+response.newRoleTitle+`","`+response.newRoleSalary+`",(SELECT id from department WHERE department_name ='`+response.newRoleDepart+`'))`, async function(err, results){
+    db.query(`INSERT INTO roles (title,salary,department_id) VALUES ("`+response.newRoleTitle+`","`+response.newRoleSalary+`",(SELECT id FROM department WHERE department_name ='`+response.newRoleDepart+`'))`, async function(err, results){
       if(err){
         console.log(err.sqlMessage);
         for(let i=0; i<5; i++) {
@@ -134,10 +145,107 @@ const addRole = () => {
      });
   });
 };
- 
-// const addEmployee = ()
+//  SQL Query to Add an Employee
+const addEmployee = () =>{
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What Is The Employee's First Name?",
+        name:"newFirstName"
+      },
+      {
+        type: "input",
+        message: "What Is The Employee's Last Name?",
+        name: "newLastName",
+      },
+      {
+        type: "input",
+        message: "What Title Will The New Employee Have?",
+        name: "newEmpRole"
+      },
+      {
+        type: "input",
+        message: "What Is The Managers Last Name?",
+        name: "newEmpManager",
+      },
+    ])
+    .then((response) =>{
+      let managerId = "Starter";
+      console.log(response);
+      db.query(`SELECT id FROM employee WHERE last_name='`+response.newEmpManager+`'`, async function(err,results){
+       managerId= results[0].id;
+     
+        db.query(`INSERT INTO employee (first_name,last_name,roles_id,manager_id) VALUES ("`+response.newFirstName+`","`+response.newLastName+`",(SELECT id FROM roles WHERE title ='`+response.newEmpRole+`'),`+managerId+`)`, async function(err, results){
+          if(err){
+            console.log(err.sqlMessage);
+            for(let i=0; i<5; i++) {
+              console.log(" ");  
+            }; 
+          };
+          if(!err){
+            console.log("New Role Added");
+            for(let i=0; i<5; i++) {
+              console.log(" ");  
+            };
+            
+          };
+          await mainMenuPrompt();
+          return;
+        });
+      });
 
-// const updateEmployee
+    });
+};
+// SQL Query to Update and Employee
+const updateEmployee = () =>{
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "First Name of the Employee To Update?",
+        name:"updateFirstName"
+      },
+      {
+        type: "input",
+        message: "Last Name of thr Employee To Update?",
+        name: "updateLastName",
+      },
+      {
+        type: "input",
+        message: "What Is The Employee's New Title?",
+        name: "updateEmpRole"
+      }
+    ])
+    .then((response) =>{
+      let rolesId = "Starter";
+      console.log(response);
+      db.query(`SELECT id FROM roles WHERE title='`+response.updateEmpRole+`'`, async function(err,results){
+       console.log(results);
+        rolesId= results[0].id;
+     
+        db.query(`UPDATE employee SET roles_id = `+rolesId+` WHERE first_name = "`+response.updateFirstName+`" AND last_name = "`+response.updateLastName+`"`, async function(err, results){
+          if(err){
+            console.log(err.sqlMessage);
+            for(let i=0; i<5; i++) {
+              console.log(" ");  
+            }; 
+          };
+          if(!err){
+            console.log("Role Updated");
+            for(let i=0; i<5; i++) {
+              console.log(" ");  
+            };
+            
+          };
+          await mainMenuPrompt();
+          return;
+        });
+      });
+
+    });
+};
 
 
+// Initial Funtion Call
 mainMenuPrompt();
